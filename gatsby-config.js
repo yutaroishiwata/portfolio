5cd0d9e65cfe8b20604cbe05c64c6f981c6a4bd6
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2020-01-17 19:37:13 +0100

    add package

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,22 @@ module.exports = {
   siteMetadata: {
     title: `Creative Portfolio`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        apiToken: process.env.DATO_API_TOKEN,
+    plugins: [
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-154111836-2",
+          head: true,
+        },
       },
-    },
-  ],
+      {
+        resolve: `gatsby-source-datocms`,
+        options: {
+          apiToken: process.env.DATO_API_TOKEN,
+        },
+      },
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-sass`,
+      `gatsby-transformer-remark`,
+    ],
 }
