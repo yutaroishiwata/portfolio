92e66d45712690742cafbcd0c22a472172618f38
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2020-02-24 18:47:54 +0100

    fix social icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Yutaro Ishiwata`,
   },
     plugins: [
       {
--- src/components/layout.js
+++ src/components/layout.js
@@ -36,77 +36,80 @@ const TemplateWrapper = ({ children }) => {
               node {
                 profileType
                 url
+                fontawesome
               }
             }
           }
         }
       `}
-      render={data => (
-        <div className={`container ${showMenu ? "is-open" : ""}`}>
-          <HelmetDatoCms
-            favicon={data.datoCmsSite.faviconMetaTags}
-            seo={data.datoCmsHome.seoMetaTags}
-          />
-          <div className="container__sidebar">
-            <div className="sidebar">
-              <h6 className="sidebar__title">
-                <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-              </h6>
-              <div
-                className="sidebar__intro"
-                dangerouslySetInnerHTML={{
-                  __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
-                }}
-              />
-              <ul className="sidebar__menu">
-                <li>
-                  <Link to="/">work</Link>
-                </li>
-                <li>
-                  <Link to="/photolog">photolog</Link>
-                </li>
-                <li>
-                  <Link to="/cv">cv</Link>
-                </li>
-              </ul>
-              <p className="sidebar__social">
-                {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-                  <a
-                    key={profile.profileType}
-                    href={profile.url}
-                    target="blank"
-                    className={`social social--${profile.profileType.toLowerCase()}`}
-                  >
-                    {" "}
-                  </a>
-                ))}
-              </p>
-              <div className="sidebar__copyright">
-                {data.datoCmsHome.copyright}
-              </div>
-            </div>
-          </div>
-          <div className="container__body">
-            <div className="container__mobile-header">
-              <div className="mobile-header">
-                <div className="mobile-header__menu">
-                  <a
-                    href="#"
-                    onClick={e => {
-                      e.preventDefault();
-                      setShowMenu(!showMenu);
-                    }}
-                  />
-                </div>
-                <div className="mobile-header__logo">
-                  <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-                </div>
-              </div>
-            </div>
-            {children}
-          </div>
-        </div>
+    render={data => (
+        <React.Fragment>
+         <div className={`overlay ${showMenu ? "is-open" : ""}`}></div>
+         <div className={`container ${showMenu ? "is-open" : ""}`}>
+           <HelmetDatoCms favicon={data.datoCmsSite.faviconMetaTags} seo={data.datoCmsHome.seoMetaTags}>
+             <script src="https://kit.fontawesome.com/6aff6489bc.js" crossorigin="anonymous"></script>    
+           </HelmetDatoCms>
+           <div className="container__sidebar">
+             <div className="sidebar">
+               <h6 className="sidebar__title">
+                 <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+               </h6>
+               <div
+                 className="sidebar__intro"
+                 dangerouslySetInnerHTML={{
+                   __html:
+                     data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                 }}
+               />
+               <ul className="sidebar__menu">
+                 <li>
+                   <Link to="/">work</Link>
+                 </li>
+                 <li>
+                   <Link to="/photolog">photolog</Link>
+                 </li>
+                 <li>
+                   <Link to="/cv">cv</Link>
+                 </li>
+               </ul>
+               <p className="sidebar__social">
+                 {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                   <a
+                     key={profile.profileType}
+                     href={profile.url}
+                     target="blank"
+                     className={`social social--${profile.profileType.toLowerCase()}`}
+                   >
+                    <i className={profile.fontawesome}></i>
+                   </a>
+                 ))}
+               </p>
+               <div className="sidebar__copyright">
+                 {data.datoCmsHome.copyright}
+               </div>
+             </div>
+           </div>
+           <div className="container__body">
+             <div className="container__mobile-header">
+               <div className="mobile-header">
+                 <div className="mobile-header__menu">
+                   <a
+                     href="#"
+                     onClick={e => {
+                       e.preventDefault();
+                       setShowMenu(!showMenu);
+                     }}
+                   />
+                 </div>
+                 <div className="mobile-header__logo">
+                   <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+                 </div>
+               </div>
+             </div>
+             {children}
+           </div>
+         </div>
+        </React.Fragment>
       )}
     />
   );
